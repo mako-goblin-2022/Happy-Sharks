74ce100a176ade7b0268f1f8e23be2dba2068b30
Kate Stretton <katestretton@Kates-MacBook-Air.local>
2022-06-23 20:28:38 +1200

    Shark WIP

--- client/components/Home.jsx
+++ client/components/Home.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react'
 
-import { getWelcome } from '../apiClient'
+import { getWelcome, getShark } from '../apiClient'
 
 function Home() {
   const [welcomeStatement, setWelcomeStatement] = useState('')
@@ -15,7 +15,30 @@ function Home() {
       })
   })
 
-  return <h1>{welcomeStatement}</h1>
-}
+  const [sharks, setSharks] = useState([])
+
+  useEffect(() => {
+    getShark()
+      .then((res) => {
+        // console.log('Res', res)
+        setSharks(res)
+      })
+      .catch((err) => {
+        console.error(err.message)
+      })
+  }, [])
+
+  return (
+  <div>
+    {/* <p>{shark[2]}</p> */}
+    {console.log('Return:', sharks[2])}
+  {/* {sharks.map((shark, i) => {
+    return (
+      <p key={i}>{shark.location}</p>
+    )
+  })} */}
+</div>
+)}
+
 
 export default Home
