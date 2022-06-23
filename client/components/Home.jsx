1f33631511c1e84d68533eeb3ac1f3dcdc40c9f3
Victoria Marchant <victoria@Victorias-MacBook-Pro.local>
2022-06-23 21:14:41 +1200

    took the welcome useeffect out and messed with sharksdata

--- client/components/Home.jsx
+++ client/components/Home.jsx
@@ -1,27 +1,17 @@
 import React, { useState, useEffect } from 'react'
 
-import { getWelcome, getShark } from '../apiClient'
+import { getShark } from '../apiClient'
 
 function Home() {
-  const [welcomeStatement, setWelcomeStatement] = useState('')
-
-  useEffect(() => {
-    getWelcome()
-      .then((res) => {
-        setWelcomeStatement(res.statement)
-      })
-      .catch((err) => {
-        console.error(err.message)
-      })
-  })
+ 
 
   const [sharks, setSharks] = useState([])
 
   useEffect(() => {
     getShark()
-      .then((res) => {
-        // console.log('Res', res)
-        setSharks(res)
+      .then((sharkData) => {
+        //?.filter shark data to contain only sharks where shark["Species Name"] contains "shark then set to state?? 
+        setSharks(sharkData)
       })
       .catch((err) => {
         console.error(err.message)
@@ -30,8 +20,20 @@ function Home() {
 
   return (
   <div>
+  <ul>
+        {sharks.map((shark, index) => (
+          <li key={index}>
+            {shark["Species Name"]}
+            <img src={shark["Species Illustration Photo"].src} alt= {shark["Species Name"]} />
+            
+            
+          </li>
+        ))}
+      </ul>
+
+
+
     {/* <p>{shark[2]}</p> */}
-    {console.log('Return:', sharks[2])}
   {/* {sharks.map((shark, i) => {
     return (
       <p key={i}>{shark.location}</p>
