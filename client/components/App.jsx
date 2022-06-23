a8a992c1eba4f090792b437a153d6c078b580610
kate-stretton <81283807+kate-stretton@users.noreply.github.com>
2022-06-23 16:33:17 +1200

    Initial component set up

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,21 +1,17 @@
-import React, { useState, useEffect } from 'react'
+import React from 'react'
 
-import { getWelcome } from '../apiClient'
+import Home from "./Home"
+
 
 function App() {
-  const [welcomeStatement, setWelcomeStatement] = useState('')
 
-  useEffect(() => {
-    getWelcome()
-      .then((res) => {
-        setWelcomeStatement(res.statement)
-      })
-      .catch((err) => {
-        console.error(err.message)
-      })
-  })
 
-  return <h1>{welcomeStatement}</h1>
+  return (
+  <>
+  <Home />
+ 
+  </>
+  )
 }
 
 export default App
